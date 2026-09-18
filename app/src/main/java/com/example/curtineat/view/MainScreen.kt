4779616362a2c3d40772f92fdade0170cdb30258
Jonathan Wong <thehackerbrother@gmail.com>
2026-09-18 16:37:41 +0800

    done bottom bar

--- app/src/main/java/com/example/curtineat/view/MainScreen.kt
+++ app/src/main/java/com/example/curtineat/view/MainScreen.kt
@@ -1,1 +1,34 @@
 package com.example.curtineat.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Menu_Bottombar(){
+    Scaffold(
+        bottomBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                IconButton(
+                    onClick = {
+                        println("Plus clicked!")
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+
+        // Your main content
+    }
+
+
+
+}
