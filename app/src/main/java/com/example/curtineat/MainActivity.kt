2fab1b6b5a40b3a07c78e9350b762bd48fb1adc8
Marilyn Emah Sim Mei Lim <simmeilim04@gmail.com>
2026-09-18 17:11:01 +0800

    third one

--- app/src/main/java/com/example/curtineat/MainActivity.kt
+++ app/src/main/java/com/example/curtineat/MainActivity.kt
@@ -11,37 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.curtineat.ui.theme.CurtinEATTheme
+import com.example.curtineat.view.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             CurtinEATTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding).padding(20.dp)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurtinEATTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/curtineat/view/MainScreen.kt
+++ app/src/main/java/com/example/curtineat/view/MainScreen.kt
@@ -1,1 +1,18 @@
 package com.example.curtineat.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text("CurtinEAT")
+    }
+}
